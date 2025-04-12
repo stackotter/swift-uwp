6e5c0089d599fc47c7f052521e1b546e54c7dc13
stackotter <stackotter@stackotter.dev>
2025-04-12 06:57:51 -0700

    Switch back to remote deps from local ones (whoops)

--- Package.swift
+++ Package.swift
@@ -8,10 +8,8 @@ let package = Package(
         .library(name: "UWP", type: .dynamic, targets: ["UWP"]),
     ],
     dependencies: [
-        // .package(url: "https://github.com/thebrowsercompany/swift-cwinrt", branch: "main"),
-        // .package(url: "https://github.com/thebrowsercompany/swift-windowsfoundation", branch: "main"),
-        .package(path: "../swift-cwinrt"),
-        .package(path: "../swift-windowsfoundation"),
+        .package(url: "https://github.com/thebrowsercompany/swift-cwinrt", branch: "main"),
+        .package(url: "https://github.com/thebrowsercompany/swift-windowsfoundation", branch: "main"),
     ],
     targets: [
         .target(
